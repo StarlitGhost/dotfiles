fe79700ac26e09d35ae3fbe9f98221b45df96243
MatthewCox <matthewcpcox@gmail.com>
2015-06-26 11:35:42 +0100

    rename all the zsh dotfiles to have .zsh extensions (for syntax highlighting on github)

---
+++ zshenv.zsh
@@ -0,0 +1,54 @@
+skip_global_compinit=1
+
+case $USER in
+	 *felix* | *sim* | *simvideo* )	export REALHOME=/user/mhc ;;
+	 * )			export REALHOME=${HOME}
+esac
+
+# source untracked machine specific environment variables, if they exist
+if [[ -e $REALHOME/.zshenv_ut ]]; then
+    source $REALHOME/.zshenv_ut
+fi
+
+export PATH=$REALHOME/.tmuxifier/bin:$PATH
+export PATH=$REALHOME/bin:$PATH
+export PATH=$REALHOME/.local/bin:$PATH
+export PATH=$REALHOME/scripts:$PATH
+export PATH=$REALHOME/.dotfiles/ignored/commands:$PATH
+typeset -U PATH
+
+export LD_LIBRARY_PATH=$REALHOME/lib:/usr/local/lib:$LD_LIBRARY_PATH
+typeset -U LD_LIBRARY_PATH
+
+case $USER in
+	*felix* | *sim* | *simvideo* )	export HISTFILE=$REALHOME/.zsh_histories/.zsh_history_$USER ;;
+	* )				export HISTFILE=$REALHOME/.zsh_history_$USER
+esac
+
+export LANG=en_GB.UTF-8
+export LC_CTYPE=en_GB.UTF-8
+unset LC_ALL
+
+export DOTFILES=$REALHOME/.dotfiles
+
+export ZSH_CUSTOM=$DOTFILES/ignored/omz-custom
+
+export EDITOR=nano
+
+# customized file/dir colours for ls
+eval `dircolors -b $REALHOME/._dircolors_`
+
+export PYTHONSTARTUP=$REALHOME/.pystartup
+
+if [[ -e $(command -v virtualenvwrapper.sh) ]]; then
+    source virtualenvwrapper.sh
+elif [[ -e /usr/bin/virtualenvwrapper.sh ]]; then
+    source /usr/bin/virtualenvwrapper.sh
+else
+    print "virtualenvwrapper.sh is missing, have you installed virtualenvwrapper?"
+fi
+
+# if environment variables need to be modified for this machine, source a file that does that
+if [[ -e $REALHOME/.zshenv_ut_post ]]; then
+    source $REALHOME/.zshenv_ut_post
+fi
