30a6333b1f7540325d902d066bba823597893e04
Matthew Cox <matthewcpcox@gmail.com>
2015-07-19 23:49:51 +0100

    gnome-terminal-obsidian: set TERM to xterm-256color and run zsh instead

--- ignored/gnome-terminal-obsidian.sh
+++ ignored/gnome-terminal-obsidian.sh
@@ -7,3 +7,4 @@ gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color 
 gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color "#222228282A2A"
 gconftool-2 -s -t float /apps/gnome-terminal/profiles/Default/background_darkness 0.98
 gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette "#111114141515:#DBDB64646464:#9191C8C86060:#FFFFCDCD2222:#64648C8CAFAF:#C4C47575CACA:#3838D1D1D1D1:#C8C8C8C8C8C8:#7F7F7F7F7F7F:#FFFF80808080:#8E8EFFFF2828:#FFFFFFFF7D7D:#8A8AC2C2F4F4:#D8D8A0A0DCDC:#8A8AF4F4F4F4:#F2F2F2F2F2F2"
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/custom_command "env TERM=xterm-256color /bin/zsh"
