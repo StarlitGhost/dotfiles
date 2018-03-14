eb9b4ded5e27de73bc7a1ca51b4ebfbfcdff3370
Matthew Cox <679547+MatthewCox@users.noreply.github.com>
2018-03-14 11:34:47 +0000

    [vimrc] Rust config

--- zshenv.zsh
+++ zshenv.zsh
@@ -77,6 +77,10 @@ eval `dircolors -b $REALHOME/._dircolors_`
 
 export PYTHONSTARTUP=$REALHOME/.pystartup
 
+if [[ -x "$(command -v rustc)" ]]; then
+    export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+fi
+
 if [[ -e $(command -v virtualenvwrapper.sh) ]]; then
     source virtualenvwrapper.sh
 elif [[ -e /usr/bin/virtualenvwrapper.sh ]]; then
