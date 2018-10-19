3b14214ab749d7e94fd19de85d5a508951a20a29
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2018-10-19 22:10:30 +0100

    [prompt] disable the time segment

--- zshrc.zsh
+++ zshrc.zsh
@@ -18,7 +18,7 @@ ZSH=$REALHOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="agnoster-tyranicmoron"
+ZSH_THEME="agnoster-starlitghost"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
