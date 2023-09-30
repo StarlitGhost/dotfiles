94668a222e8eae2e4b47d37dd80f24ac3b713cb3
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-09-30 13:56:49 +0100

    [zshenv] add cargo env for rust

--- zshenv.zsh
+++ zshenv.zsh
@@ -65,6 +65,7 @@ export PYTHONSTARTUP=$HOME/.pystartup
 
 if [[ -x "$(command -v rustc)" ]]; then
     export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+    source "$HOME/.cargo/env"
 fi
 
 if [[ -e $(command -v virtualenvwrapper.sh) ]]; then
