8d95396ada6936b1d0f1ca0daa1da6b8f1ae5b07
StarlitGhost <679547+StarlitGhost@users.noreply.github.com>
2023-09-30 17:55:01 +0100

    [zshenv] only source cargo env if it exists

--- zshenv.zsh
+++ zshenv.zsh
@@ -69,7 +69,9 @@ export PYTHONSTARTUP=$HOME/.pystartup
 
 if [[ -x "$(command -v rustc)" ]]; then
     export RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
-    source "$HOME/.cargo/env"
+    if [[ -e $HOME/.cargo/env ]]; then
+        source "$HOME/.cargo/env"
+    fi
 fi
 
 if [[ -e $(command -v virtualenvwrapper.sh) ]]; then
